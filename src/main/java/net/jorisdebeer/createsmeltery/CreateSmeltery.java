605006ce240ce76944be868af0942c9de2d4d474
jorisdebeer <jorisvkeulen@gmail.com>
2023-09-27 21:27:51 +0200

    Initial commit

---
+++ src/main/java/net/jorisdebeer/createsmeltery/CreateSmeltery.java
@@ -0,0 +1,18 @@
+package net.jorisdebeer.createsmeltery;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreateSmeltery implements ModInitializer {
+
+	public static final String MOD_ID = "createsmeltery";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/net/jorisdebeer/createsmeltery/CreateSmelteryClient.java
@@ -0,0 +1,10 @@
+package net.jorisdebeer.createsmeltery;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreateSmelteryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
